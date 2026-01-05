2eb56876564b4b08a88ff4c3d7226182b4998242
BdzUcas <bracken.zabriskie@ucas-edu.net>
2026-01-05 10:50:10 -0700

    nonsense

--- notes/lists.py
+++ notes/lists.py
@@ -1,2 +1,15 @@
 #BZ2 Types of lists notes
 #This is a change to make it so i can commit and push
+#UNLIMITED POWER!!!
+#This is a comment
+#This is another comment
+#Bufallo buffalo buffalo Buffalo buffalo
+#^That is gramatically correct
+#your welcome
+#^That isn't
+#G
+#Gi
+#Git
+#Gith
+#Githu
+#Github
