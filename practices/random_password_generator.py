663af64a783956dab87e9676aaaed3b7fe9039a1
BdzUcas <bracken.zabriskie@ucas-edu.net>
2026-01-22 19:45:58 +0000

    worked on random password generator

--- practices/random_password_generator.py
+++ practices/random_password_generator.py
@@ -1,8 +1,9 @@
 #BZ 2nd Random Password Generator
+#import libraries
+import random as r
 def count(start, end):
     return list(range(start, end + 1))
 specials = count(33, 47) + count(58, 64) + count(91,96) + count(123,126)
-
 def add_character(characters, nums_required, special_required, up_required):
     nums = 0
     special = 0
@@ -15,5 +16,11 @@ def add_character(characters, nums_required, special_required, up_required):
             special += 1
         if char in count(65,90):
             upper += 1
-    
+    potentials = count(97,122)
+    if nums < nums_required:
+        potentials.extend(count(48,57))
+    if special < special_required:
+        potentials.extend(specials)
+    if upper < up_required:
+        potentials.extend(count(65,90))
     
