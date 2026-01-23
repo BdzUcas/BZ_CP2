e1852cb52afc4e14e3d9b8d37d2d96dd352f0ba6
BdzUcas <bracken.zabriskie@ucas-edu.net>
2026-01-23 10:50:09 -0700

    finished random password generator

--- practices/random_password_generator.py
+++ practices/random_password_generator.py
@@ -8,46 +8,39 @@ def int_input():
         except:
             print('Please input a number!')
             continue
-        
+def yes_no():
+    while True:
+        choice = input('Y/N > ').lower() 
+        if choice == 'n':
+            return False
+        elif choice == 'y':
+            return True
+        else:
+            print('Input Y or N!')
 def count(start, end):
     return list(range(start, end + 1))
 specials = count(33, 47) + count(58, 64) + count(91,96) + count(123,126)
-def add_character(characters, nums_required, special_required, up_required):
-    nums = 0
-    special = 0
-    upper = 0
-    for char in characters:
-        if char in count(48,57):
-            nums += 1
-        if char in specials:
-            special += 1
-        if char in count(65,90):
-            upper += 1
+def add_character(nums_required, special_required, up_required):
     potentials = count(97,122)
-    if nums < nums_required:
+    if nums_required:
         potentials.extend(count(48,57))
-    if special < special_required:
+    if special_required:
         potentials.extend(specials)
-    if upper < up_required:
+    if up_required:
         potentials.extend(count(65,90))
     return r.choice(potentials)
 def main():
     characters = []
     print('What length should it be?')
     length = int_input()
-    print('How many uppercase letters are required?')
-    up_required = int_input()
-    print('How many numbers are required?')
-    nums_required = int_input()
-    print('How many special characters are required?')
-    special_required = int_input()
-    needed_len = up_required + nums_required + special_required
-    if needed_len < length:
-        needed_len = length
-    else:
-        needed_len += r.randint(0,5)
-    while len(characters) < needed_len:
-        characters.append(add_character(characters, nums_required, special_required, up_required))
+    print('Are uppercase letters required?')
+    up_required = yes_no()
+    print('Are numbers required?')
+    nums_required = yes_no()
+    print('Are special characters required?')
+    special_required = yes_no()
+    while len(characters) < length:
+        characters.append(add_character(nums_required, special_required, up_required))
     password = ''
     for char in [chr(i) for i in characters]: password = password + char
     print(password)
