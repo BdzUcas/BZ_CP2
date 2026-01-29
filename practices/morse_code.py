df0a459f264c5242de8a4010027e0fc3182b6d35
BdzUcas <bracken.zabriskie@ucas-edu.net>
2026-01-29 21:36:42 +0000

    worked on morse code

--- practices/morse_code.py
+++ practices/morse_code.py
@@ -1,3 +1,34 @@
 #BZ 2nd Morse Code Translator
-alphabet = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
-morse = ('.-','-...','-.-.','-..','.','..-.','--.','....','..','.---','-.-','.-..','--','-.','---')
+#Create tuples to store data
+alphabet = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9',' ')
+morse = ('.-','-...','-.-.','-..','.','..-.','--.','....','..','.---','-.-','.-..','--','-.','---','.--.','--.-','.-.','...','-','..-','...-','.--','-..-','-.--','--..','.----','..---','...--','....-','.....','-....','--...','---..','----.','-----','/')
+def translate(char):
+    if char in alphabet:
+        pos = alphabet.index(char)
+        return morse[pos]
+    elif char in morse:
+        pos = morse.index(char)
+        return alphabet[pos]
+    else:
+        return 'error'
+def validate(string):
+    for char in string:
+        all_chars = alphabet + morse
+        if not char in all_chars:
+            return False
+    return True
+def to_morse(string):
+    if not validate(string):
+        return 'You can only use letters and numbers!'
+    translated = ''
+    for char in string:
+        translated = translated + translate(char) + ' '
+    return translated
+def to_alpha(string):
+    string = string.split(' ')
+    translated = ''
+    for char in string:
+        translated = translated + translate(char) + ''
+    return translated
+print(to_alpha(to_morse('jingle bells jingle bells jingle all the way')))
+    
